6499b7155430b1e6937e9fc007704be6f825971b
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2015-12-04 14:38:33 -0500

    previous fix again

--- app/scripts/GoogleImages.js
+++ app/scripts/GoogleImages.js
@@ -4,7 +4,7 @@ export default (query) => {
   return axios({
     method: 'get',
     // Note: this is VERY insecure (relying on random 3rd party, cors.io)
-    url: 'http://api.pixplorer.co.uk/image?amount=4&word='+encodeURIComponent(query)
+    url: 'http://api.pixplorer.co.uk/image?amount=4&word='+encodeURIComponent(query + ' insect')
   }).catch(response => {
     console.log('count not retrieve google images', response);
   });
